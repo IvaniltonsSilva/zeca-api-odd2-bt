f981eef317e50ca05c790424cc1b0c4cd315c2e4
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-20 21:15:28 -0300

    chore: update shareCode to E4P04TDUYKF

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5n4o0boe?shareCode=N3K4I57VUF2&afp8=tapi&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5n4o0boe?shareCode=E4P04TDUYKF&afp8=tapi&home=1' });
     res.end();
   }
