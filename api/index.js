92ba53a3d4ea8a0ec7ae026f6c7a7dd02e7e89b9
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-26 18:56:13 -0300

    chore: update shareCode to IHBS8PMNAG8

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5n4o0boe?shareCode=RE1AA9ENRBV&afp8=tapi&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5n4o0boe?shareCode=IHBS8PMNAG8&afp8=tapi&home=1' });
     res.end();
   }
